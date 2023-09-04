260ca638e157e4040f1293e7ae4f8cee09f4845d
Marcelo <marceloaborges@yahoo.com.br>
2023-09-03 22:04:16 -0300

    teste

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -84,7 +84,8 @@
 
     {{-- Base Scripts --}}
     @if(!config('adminlte.enabled_laravel_mix'))
-        <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+        {{-- <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script> --}}
+        {{-- <script src="{{ asset('js/jquery.js') }}"></script> --}}
         <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
         <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
         <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>        
